9cdf7ae6c1a8db1c38ff0f393cc6650363d63caa
umesh jangid <jangidumesh95@gmail.com>
2017-06-19 12:24:31 +0530

    hpcl petrol and deisel price

--- project/mainapp/urls.py
+++ project/mainapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^get_cities/', views.get_cities),
     url(r'^get_rate/', views.get_rate),
     url(r'^get_nearest/', views.nearest_city),
+    url(r'^get_hpcl/', views.hpcl),
 
 ]
